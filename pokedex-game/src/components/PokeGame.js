b5404f352c24804e98331dcc79976bd24b61c56c
Tim Sherry <tsherry@tjs-mbp-2.home>
2020-07-06 18:23:20 -0400

    updated pokemon and stats to make it more competitive

--- pokedex-game/src/components/PokeGame.js
+++ pokedex-game/src/components/PokeGame.js
@@ -4,14 +4,14 @@ import Pokedex from './Pokedex'
 class Pokegame extends Component {
     static defaultProps = {
         pokemon : [
-            { id: 4, name: 'Charmander', type: 'fire', exp: 62 },
-            { id: 7, name: 'Squirtle', type: 'water', exp: 63 },
-            { id: 11, name: 'Metapod', type: 'bug', exp: 72 },
-            { id: 12, name: 'Butterfree', type: 'flying', exp: 178 },
-            { id: 25, name: 'Pikachu', type: 'electric', exp: 112 },
-            { id: 39, name: 'Jigglypuff', type: 'normal', exp: 95 },
-            { id: 94, name: 'Genger', type: 'ghost', exp: 225 },
-            { id: 133, name: 'Eevee', type: 'normal', exp: 65 }
+            { id: 6, name: 'Charizard', type: 'fire', exp: 162 },
+            { id: 9, name: 'Blastoise', type: 'water', exp: 163 },
+            { id: 3, name: 'Venasaur', type: 'grass', exp: 162 },
+            { id: 149, name: 'Dragonite', type: 'dragon', exp: 170 },
+            { id: 25, name: 'Pikachu', type: 'electric', exp: 180 },
+            { id: 150, name: 'Mewtwo', type: 'normal', exp: 179 },
+            { id: 94, name: 'Genger', type: 'ghost', exp: 165 },
+            { id: 143, name: 'Snorlax', type: 'normal', exp: 165 }
         ]
     }
     render() {
